ec08d629f96bcf2476736b4e7ffd8338a27a4f6c
Pierre-Anthony Lemieux <pal@palemieux.com>
2021-12-05 11:13:24 -0800

    Improve prepare patches

--- imf-prepare-patches.sh
+++ imf-prepare-patches.sh
@@ -51,7 +51,7 @@ GPLCC=" * This file is part of FFmpeg.\n\
 sed -i "s/^ \* This file is part of FFmpeg\./$GPLCC/" $PATCHES_SRC
 
 # remove tests from Makefile
-sed -i "/^TESTPROGS-\$\(CONFIG_IMF_DEMUXER\)/,+1 d" $PATCHES_MAKEFILE
+sed -i "/^TESTPROGS-\$(CONFIG_IMF_DEMUXER)/d" $PATCHES_MAKEFILE
 
 git add -- $PATCHES_SRC $PATCHES_MISC $PATCHES_MAKEFILE
 git commit -m "${PATCH_NAME}: Demuxer" -- $PATCHES_SRC $PATCHES_MISC $PATCHES_MAKEFILE
@@ -86,7 +86,7 @@ CHANGE NOTES:
 
 # reset the makefile
 git checkout -f $PATCH_BRANCH -- $PATCHES_MAKEFILE
-
+git reset $BASE_BRANCH -- $PATCHES_MAKEFILE
 git add -- $PATCHES_IMF_TESTS $PATCHES_MAKEFILE
 git commit -m "${PATCH_NAME}: Tests" -- $PATCHES_IMF_TESTS $PATCHES_MAKEFILE
 git notes add -m "Tests for the IMF demuxer."
