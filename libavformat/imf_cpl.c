ddf7daafb243af1aeae17dc3bcf4eb7a708f444e
Pierre-Anthony Lemieux <pal@palemieux.com>
2021-12-14 08:29:07 -0800

    Enforce MXF track files Fix stealth assignment in if statement Remove unusued function param

--- libavformat/imf_cpl.c
+++ libavformat/imf_cpl.c
@@ -731,13 +731,14 @@ int ff_imf_parse_cpl(AVIOContext *in, FFIMFCPL **cpl)
     filesize = filesize > 0 ? filesize : 8192;
     av_bprint_init(&buf, filesize + 1, AV_BPRINT_SIZE_UNLIMITED);
     ret = avio_read_to_bprint(in, &buf, UINT_MAX - 1);
-    if (ret < 0 || !avio_feof(in) || (filesize = buf.len) == 0) {
+    if (ret < 0 || !avio_feof(in) || buf.len == 0) {
+        av_log(NULL, AV_LOG_ERROR, "Cannot read IMF CPL\n");
         if (ret == 0) {
-            av_log(NULL, AV_LOG_ERROR, "Cannot read IMF CPL\n");
-            return AVERROR_INVALIDDATA;
+            ret = AVERROR_INVALIDDATA;
         }
     } else {
         LIBXML_TEST_VERSION
+        filesize = buf.len;
         doc = xmlReadMemory(buf.str, filesize, NULL, NULL, 0);
         if (!doc) {
             av_log(NULL,
