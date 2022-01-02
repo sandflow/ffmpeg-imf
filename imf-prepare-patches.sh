8539e67872ee010e01727b0a40d907281a0b88f0
Pierre-Anthony Lemieux <pal@palemieux.com>
2022-01-02 09:31:37 -0800

    Added missing file to patches

--- imf-prepare-patches.sh
+++ imf-prepare-patches.sh
@@ -27,11 +27,12 @@ git reset $BASE_BRANCH
 
 git add -- $PATCHES_ALL
 git commit -m "$PATCH_MSG" -- $PATCHES_ALL
-git notes add -m "The following files need to be added to FATE samples:
+git notes add -m "The following files would need to be added to FATE samples:
     http://ffmpeg-imf-samples-public.s3-website-us-west-1.amazonaws.com/countdown/ASSETMAP.xml
     http://ffmpeg-imf-samples-public.s3-website-us-west-1.amazonaws.com/countdown/CPL_f5095caa-f204-4e1c-8a84-7af48c7ae16b.xml
     http://ffmpeg-imf-samples-public.s3-website-us-west-1.amazonaws.com/countdown/PKL_4671220f-c87a-4660-bf2a-6ef848791a2c.xml
     http://ffmpeg-imf-samples-public.s3-website-us-west-1.amazonaws.com/countdown/VOLINDEX.xml
+    http://ffmpeg-imf-samples-public.s3-website-us-west-1.amazonaws.com/countdown/frame-counter.mxf
 "
 
 git format-patch -o $PATCHES_DIR -v $PATCH_VERSION --notes -s $BASE_BRANCH
