fa3a317455a3f46133cccae506aaf73c9b56c5ca
Pierre-Anthony Lemieux <pal@palemieux.com>
2022-01-07 15:05:43 -0800

    Simplify fate-imf dependencies

--- imf-prepare-patches.sh
+++ imf-prepare-patches.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-PATCH_VERSION="3"
+PATCH_VERSION="4"
 
 PATCH_MSG="avformat/imf: add IMF CPL with repeated resources to FATE"
 
@@ -29,10 +29,10 @@ git add -- $PATCHES_ALL
 git commit -m "$PATCH_MSG" -- $PATCHES_ALL
 git notes add -m "The \`fate-imf-cpl-with-repeat\` target requires following files (<256 kB)
 to placed under \$(TARGET_SAMPLES)/imf/countdown/:
-    http://ffmpeg-imf-samples-public.s3-website-us-west-1.amazonaws.com/countdown/ASSETMAP.xml
-    http://ffmpeg-imf-samples-public.s3-website-us-west-1.amazonaws.com/countdown/CPL_f5095caa-f204-4e1c-8a84-7af48c7ae16b.xml
-    http://ffmpeg-imf-samples-public.s3-website-us-west-1.amazonaws.com/countdown/PKL_4671220f-c87a-4660-bf2a-6ef848791a2c.xml
-    http://ffmpeg-imf-samples-public.s3-website-us-west-1.amazonaws.com/countdown/frame-counter.mxf
+    http://ffmpeg-imf-samples-public.s3-website-us-west-1.amazonaws.com/countdown-small/ASSETMAP.xml
+    http://ffmpeg-imf-samples-public.s3-website-us-west-1.amazonaws.com/countdown-small/CPL_bb2ce11c-1bb6-4781-8e69-967183d02b9b.xml
+    http://ffmpeg-imf-samples-public.s3-website-us-west-1.amazonaws.com/countdown-small/PKL_c8f6716b-0dfa-4062-8569-98fc77637287.xml
+    http://ffmpeg-imf-samples-public.s3-website-us-west-1.amazonaws.com/countdown-small/countdown-small.mxf
 "
 
 git format-patch -o $PATCHES_DIR -v $PATCH_VERSION --notes -s $BASE_BRANCH
